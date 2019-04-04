b5d78d47c4821a1d67588634d523efaa39aef3d1
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro.local>
2019-04-04 12:00:52 -0700

    POL system responds to polcontroller updates

--- workers/unity/Assets/Fps/Scripts/GameLogic/PolSystem.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/PolSystem.cs
@@ -14,6 +14,7 @@ namespace Pol
         {
             public readonly int Length;
             public ComponentDataArray<Pol.PolEntityData.Component> PolEntityDataComponents;
+            [ReadOnly] public ComponentDataArray<Pol.PolController.ReceivedUpdates> Updates;
         }
 
         [Inject] private Data data;
@@ -22,9 +23,18 @@ namespace Pol
         {
             for (var i = 0; i < data.Length; ++i)
             {
-                var component = data.PolEntityDataComponents[i];
-                component.Status = component.Status + 1;
-                data.PolEntityDataComponents[i] = component;
+                var updates = data.Updates[i].Updates;
+                foreach (var update in updates)
+                {
+                    //assuming here there will only be one POLController update every 3 seconds, because of the logic in the POlController
+                    //if there are more frequent updates, will need to change logic to stagger resulting updates to POL EntityData
+                    for (var j = 0; j < data.Length; ++j)
+                    {
+                        var component = data.PolEntityDataComponents[i];
+                        component.Status = update.RobotsActive * 2;
+                        data.PolEntityDataComponents[i] = component;
+                    }
+                }
             }
         }
     }
